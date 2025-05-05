ae7e1df673caac9e507f7626df0fd87bc9c40a28
Reynaldo-Allison <146046671+Reynaldo-Allison@users.noreply.github.com>
2025-05-05 00:48:13 -0400

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,8 +1,14 @@
-from backend.app import create_app, db
+import sys
+from os.path import abspath, dirname
+
+# Add project root to Python path
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+
+from app import create_app, db
 from flask_migrate import Migrate
 
 app = create_app()
-migrate = Migrate(app, db)  # This enables 'flask db' commands
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run()
