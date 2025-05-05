728e834cb95a349d94fd3b9fe4211af2cfa33b90
Reynaldo-Allison <146046671+Reynaldo-Allison@users.noreply.github.com>
2025-05-04 23:48:27 -0400

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from .config import Config
+from app.config import Config
 from flask_cors import CORS
 
 db = SQLAlchemy()
